8a7b2c787db66855b05612daea5780d93517902f
yuriangelsena-analistadedatos <yuriangelcatalina@gmail.com>
2024-11-23 00:09:17 -0500

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,13 +5,13 @@ describe('API Endpoints', () => {
   
  it('should return a welcome message', async () => {
  const res = await request(app).get('/');
- expect(res.statusCode).toEqual(200);
+ expect(res.statusaCode).toEqual(200);
  expect(res.text).toBe('Bienvenido a la API');
  });
   
  it('should return the status', async () => {
  const res = await request(app).get('/status');
- expect(res.statusaCode).toEqual(200);
+ expect(res.statusCode).toEqual(200);
  expect(res.body).toEqual({ status: 'API funcionando correctamente' });
  });
   
